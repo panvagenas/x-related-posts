b53ce58120bab40b44df4b1dc5ae563602a78c12
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-05-18 23:11:25 +0300

    ...

--- templates/main/carousel.php
+++ templates/main/carousel.php
@@ -117,7 +117,7 @@ $sliderContainerUID = uniqid('xrp-slider-container-');
                 $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
                 $ChanceToShow: 1,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
                 $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-                $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+                $Steps: showPiecesMin                                       //[Optional] Steps to go for each navigation request, default value is 1
             }
         };
         var jssor_slider1 = new $JssorSlider$(sliderContainerID, options);
